f297deaf0f64aa7d7e311763b8a5eb4dc3d11d43
Alexander Ott <contact@alexanderott.at>
2016-10-15 18:07:46 +0200

    Set german language in numbro library only once

--- src/app/core/pipes/number.pipe.ts
+++ src/app/core/pipes/number.pipe.ts
@@ -3,47 +3,48 @@ import {TranslateService} from "ng2-translate";
 var numbro = require('numbro');
 import {numbro} from "numbro";
 
+
+let deLang = {
+    langLocaleCode: 'de',
+    cultureCode: 'de',
+    delimiters: {
+        thousands: '.',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal: function () {
+        return '.';
+    },
+    currency: {
+        symbol: '€',
+        position: 'postfix'
+    },
+    defaults: {
+        currencyFormat: ',4 a'
+    },
+    formats: {
+        fourDigits: '4 a',
+        fullWithTwoDecimals: ',0.00 $',
+        fullWithTwoDecimalsNoCurrency: ',0.00',
+        fullWithNoDecimals: ',0 $'
+    }
+};
+
+numbro.culture('de', deLang);
+
 @Pipe({name: 'nvryNumber'})
 export class NumberPipe implements PipeTransform {
 
-    private deLang = {
-        langLocaleCode: 'de',
-        cultureCode: 'de',
-        delimiters: {
-            thousands: '.',
-            decimal: ','
-        },
-        abbreviations: {
-            thousand: 'k',
-            million: 'm',
-            billion: 'b',
-            trillion: 't'
-        },
-        ordinal: function () {
-            return '.';
-        },
-        currency: {
-            symbol: '€',
-            position: 'postfix'
-        },
-        defaults: {
-            currencyFormat: ',4 a'
-        },
-        formats: {
-            fourDigits: '4 a',
-            fullWithTwoDecimals: ',0.00 $',
-            fullWithTwoDecimalsNoCurrency: ',0.00',
-            fullWithNoDecimals: ',0 $'
-        }
-    };
-
     constructor(private translate: TranslateService) {
+        numbro.culture(this.translate.currentLang);
     }
 
     transform(value: number, numberOfDecimals: number = 2): string {
-        numbro.culture('de', this.deLang);
-        numbro.culture(this.translate.currentLang);
-
         var format = '0,0.';
         for (var i = 0; i < numberOfDecimals; i++) {
             format += '0';
