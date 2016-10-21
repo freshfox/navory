dfccf9b7e99a9aa4b72dbbf84eb02e1892eedbda
Alexander Ott <contact@alexanderott.at>
2016-10-21 18:13:29 +0200

    Implement dashboard

--- src/app/internal/dashboard/dashboard.component.ts
+++ src/app/internal/dashboard/dashboard.component.ts
@@ -1,8 +1,73 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
+import {ReportService} from "../../services/report.service";
+var moment = require('moment');
+import * as Chartist from 'chartist';
 
 @Component({
-	template: `Dashboard`
+    templateUrl: 'dashboard.component.html'
 })
 export class DashboardComponent {
+
+    private data;
+    private loading: boolean = false;
+
+    constructor(private reportService: ReportService, private el: ElementRef) {
+
+    }
+
+    ngAfterViewInit() {
+        this.loading = true;
+        this.reportService.getFinanceOverview()
+            .subscribe((data) => {
+                this.loading = false;
+                this.data = data;
+                this.showChart();
+            });
+    }
+
+    showChart() {
+        var income = [];
+        var expenses = [];
+        this.data.months.forEach((month) => {
+            income.push(month.income.totalNet);
+            expenses.push(month.expense.totalNet);
+        });
+
+        var chart = new Chartist.Line('.ct-chart', {
+                labels: moment.monthsShort(),
+                series: [
+                    income,
+                    expenses
+                ]
+            }, {
+                height: '240px',
+                lineSmooth: Chartist.Interpolation.simple({
+                    divisor: 2
+                }),
+                chartPadding: {
+                    left: 40
+                },
+                low: 0,
+                fullWidth: true,
+                showArea: true,
+                axisY: {
+                    onlyInteger: true,
+                    offset: 20,
+                    labelInterpolationFnc: function(value) {
+                        return '&euro;' + value;
+                    },
+                },
+                axisX: {
+                    showGrid: false
+                }
+            }
+        );
+
+        chart.on('draw', function(data) {
+            if (data.type === 'line' || data.type === 'area') {
+
+            }
+        });
+    }
 
 }
--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,11 +1,13 @@
 import {BaseService} from "./base.service";
 import {Http} from "@angular/http";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class ReportService extends BaseService {
 
     private pathVatReport = '/reports/vat';
+    private pathFinance = '/finance';
 
     constructor(http: Http) {
         super(http);
@@ -15,6 +17,10 @@ export class ReportService extends BaseService {
         let path = `${this.pathVatReport}/?quarter=${quarter}&year=${year}`;
 
         return this.get(path);
+    }
+
+    getFinanceOverview() {
+        return this.get(this.pathFinance);
     }
 
 }
