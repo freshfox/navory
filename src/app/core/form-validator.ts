f77362ce4a3cb218d7d8fab706496a6af08234fe
Alexander Ott <contact@alexanderott.at>
2016-10-26 16:41:15 +0200

    Add validator for numeric fields

--- src/app/core/form-validator.ts
+++ src/app/core/form-validator.ts
@@ -14,7 +14,8 @@ export class FormValidator {
             'passwordsNotEqual': 'Passwörter stimmen nicht überein.',
             'minlength': `Mindestlänge ${validatorValue.requiredLength} Zeichen`,
             'invalidDate': 'Bitte gib ein korrektes Datum ein',
-            'userWithEmailExists': 'Ein Benutzer mit dieser E-Mail Adresse ist bereits in unserem System vorhanden.'
+            'userWithEmailExists': 'Ein Benutzer mit dieser E-Mail Adresse ist bereits in unserem System vorhanden.',
+            'notNumeric': 'Bitte gib eine Zahl ein'
         };
 
         return config[validatorName] || 'No error message for type ' + validatorName;
@@ -36,6 +37,17 @@ export class FormValidator {
         if(!date.isValid()) {
             return {
                 invalidDate: true
+            };
+        }
+
+        return null;
+    }
+
+    static number(control: FormControl) {
+        let value = `${control.value}`;
+        if(value && !validator.isNumeric(value)) {
+            return {
+                notNumeric: true
             };
         }
 
