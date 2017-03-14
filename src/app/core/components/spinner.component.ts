0c81b3cb9a9c6c03a4c1c6e9733571f7221dfbc9
Alexander Ott <contact@alexanderott.at>
2017-03-14 12:59:02 +0100

    Improve reliability of spinner component

--- src/app/core/components/spinner.component.ts
+++ src/app/core/components/spinner.component.ts
@@ -1,19 +1,30 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, SimpleChange, OnDestroy} from "@angular/core";
 import {Input} from "@angular/core/src/metadata/directives";
 
 @Component({
 	selector: 'nvry-spinner',
 	template: `
-        <md-progress-circle [mode]="progress ? 'determinate' : 'indeterminate'" [value]="progress">`
+		<md-progress-circle mode="indeterminate" *ngIf="progress >= 100 || progress === undefined"></md-progress-circle>
+        <md-progress-circle mode="determinate" [value]="progress" *ngIf="progress > 0 && progress < 100"></md-progress-circle>`
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit, OnDestroy {
 
 	@Input() progress: number;
+
+	private interval;
 
 	constructor() {
 	}
 
 	ngOnInit() {
+		this.interval = setInterval(() => {
+			return this.progress;
+		}, 200);
+	}
+
+	ngOnDestroy() {
+		clearInterval(this.interval);
+		this.interval = null;
 	}
 
 }
