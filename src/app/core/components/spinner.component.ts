03f55268e80787f87b87a0a011237b9c5a37de0f
Alexander Ott <contact@alexanderott.at>
2017-04-15 22:11:59 +0200

    Upgrade @angular/material to beta.3

--- src/app/core/components/spinner.component.ts
+++ src/app/core/components/spinner.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit, SimpleChange, OnDestroy, Input} from "@angular/core";
+import {Component, Input, OnDestroy, OnInit} from "@angular/core";
 
 @Component({
 	selector: 'nvry-spinner',
 	template: `
-		<md-progress-circle mode="indeterminate" *ngIf="progress >= 100 || progress === undefined"></md-progress-circle>
-        <md-progress-circle mode="determinate" [value]="progress" *ngIf="progress > 0 && progress < 100"></md-progress-circle>`
+		<md-progress-spinner mode="indeterminate" *ngIf="progress >= 100 || progress === undefined"></md-progress-spinner>
+		<md-progress-spinner mode="determinate" [value]="progress"
+							*ngIf="progress > 0 && progress < 100"></md-progress-spinner>`
 })
 export class SpinnerComponent implements OnInit, OnDestroy {
 
