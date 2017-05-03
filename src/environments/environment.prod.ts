f64dc5e7a2b87692de4cea931550c63d7ff2386a
Alexander Ott <contact@alexanderott.at>
2017-05-03 16:43:06 +0200

    Add Braintree production merchant ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	apiUrl: 'https://api.navory.com',
 	intercomAppId: 'ddlcldls',
-	braintreeMerchantId: '',
+	braintreeMerchantId: '3czr4h4k9sp7zj7z',
 };
