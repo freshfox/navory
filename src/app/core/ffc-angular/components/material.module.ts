005eb84d909ba5dce9912ed1c6eaebc86fce2dc4
Alexander Ott <contact@alexanderott.at>
2017-06-22 12:20:05 +0200

    Include ffc-angular as submodule

--- src/app/core/ffc-angular/components/material.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {MdProgressSpinnerModule, MdTooltipModule} from "@angular/material";
-
-@NgModule({
-    imports: [BrowserModule, MdProgressSpinnerModule, MdTooltipModule],
-})
-export class FFMaterialModule {
-}
